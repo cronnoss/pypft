20aeb3748847a68e62c30edb1b00d5a663b31f41
Evgeniy Karasev <karasevevg@gmail.com>
2019-11-05 01:19:18 +1000

    add methods for add_group

--- test_add_group.py
+++ test_add_group.py
@@ -22,16 +22,20 @@ class test_add_group(unittest.TestCase):
 
     def test_test_add_group(self):
         wd = self.wd
-        # open home page
-        wd.get("http://localhost/addressbook/")
-        # login
-        wd.set_window_size(1196, 797)
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").send_keys("admin")
-        wd.find_element(By.NAME, "pass").send_keys("secret")
-        wd.find_element(By.CSS_SELECTOR, "input[type=\'submit\']").click()
-        # open groups page
-        wd.find_element(By.LINK_TEXT, "groups").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element(By.LINK_TEXT, "group page").click()
+
+    def create_group(self, wd):
         # init group creation
         wd.find_element(By.NAME, "new").click()
         # fill group form
@@ -43,10 +47,19 @@ class test_add_group(unittest.TestCase):
         wd.find_element(By.NAME, "group_footer").send_keys("pytest3")
         # submit group creation
         wd.find_element(By.NAME, "submit").click()
-        # return to groups page
-        wd.find_element(By.LINK_TEXT, "group page").click()
-        # logout
-        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def open_groups_page(self, wd):
+        wd.find_element(By.LINK_TEXT, "groups").click()
+
+    def login(self, wd):
+        wd.set_window_size(1196, 797)
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").send_keys("admin")
+        wd.find_element(By.NAME, "pass").send_keys("secret")
+        wd.find_element(By.CSS_SELECTOR, "input[type=\'submit\']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def tearDown(self):
         self.wd.quit()
