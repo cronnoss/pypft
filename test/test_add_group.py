7c66fd3975eecda6193061acc56a4086490cf727
Evgeniy Karasev <karasevevg@gmail.com>
2019-11-05 17:05:59 +1000

    session management class is highlighted1

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.session.create_group(Group(name="pytest1", header="pytest2", footer="pytest3"))
+    app.create_group(Group(name="pytest1", header="pytest2", footer="pytest3"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.session.create_group(Group(name="", header="", footer=""))
+    app.create_group(Group(name="", header="", footer=""))
     app.session.logout()
