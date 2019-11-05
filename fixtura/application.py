1d2d9db79454897feb18bc968ebe1bbe48a0b31a
Evgeniy Karasev <karasevevg@gmail.com>
2019-11-05 18:38:16 +1000

    class for working with groups is highlighted

--- fixtura/application.py
+++ fixtura/application.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.by import By
+
+from fixtura.group import GroupHelper
 from fixtura.session import SessionHelper
 
 
@@ -9,30 +10,7 @@ class Application:
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation.
-        wd.find_element(By.NAME, "new").click()
-        # fill group form
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").click()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element(By.NAME, "submit").click()
-        self.return_to_groups_page()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "groups").click()
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 import pytest
 
-from model.group import Group
 from fixtura.application import Application
+from model.group import Group
 
 
 @pytest.fixture()
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="pytest1", header="pytest2", footer="pytest3"))
+    app.group.create(Group(name="pytest1", header="pytest2", footer="pytest3"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
