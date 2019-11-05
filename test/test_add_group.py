47feea9499c012017c2b46798817dad72af3fbd0
Evgeniy Karasev <karasevevg@gmail.com>
2019-11-05 16:32:31 +1000

    transition to pytest completed. created packages for different areas of responsibility.

---
+++ test/test_add_group.py
@@ -0,0 +1,23 @@
+import pytest
+
+from model.group import Group
+from fixtura.application import Application
+
+
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="pytest1", header="pytest2", footer="pytest3"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
