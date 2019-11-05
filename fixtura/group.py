aad619b7f0e46d92fd2966fb80da54ed9b469245
Evgeniy Karasev <karasevevg@gmail.com>
2019-11-05 19:15:54 +1000

    class for deleting first group is highlighted

--- fixtura/group.py
+++ fixtura/group.py
@@ -26,6 +26,15 @@ class GroupHelper:
         wd.find_element(By.NAME, "submit").click()
         self.return_to_groups_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element(By.NAME, "Select ()").click()
+        # submit deletion
+        wd.find_element(By.NAME, "delete").click()
+        self.return_to_groups_page()
+
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element(By.LINK_TEXT, "groups").click()
