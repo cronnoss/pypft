96194ad72eeaec19054f52baa176bbaad0f32abd
cronnoss <karasevevg@gmail.com>
2019-11-11 22:08:06 +1000

    implement ensure_login testing

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,6 +3,8 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="pytest1", header="pytest2", footer="pytest3"))
+#   for testing ensure_login
+#    app.session.logout()
 
 
 def test_add_empty_group(app):
