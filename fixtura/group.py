3536cd77a54cc97e5b5922ce680ac3d3aa39a799
cronnoss <karasevevg@gmail.com>
2019-11-12 19:46:30 +1000

    implement randomization

--- fixtura/group.py
+++ fixtura/group.py
@@ -36,9 +36,12 @@ class GroupHelper:
             wd.find_element(By.NAME, field_name).send_keys(text)
 
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element(By.NAME, "delete").click()
         self.return_to_groups_page()
@@ -48,10 +51,17 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element(By.XPATH, "//input[@name='selected[]']").click()
 
-    def modify_first_group(self, new_group_data):
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements(By.XPATH, "//input[@name='selected[]']")[index].click()
+
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
+
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open modification form
         wd.find_element(By.NAME, "edit").click()
         # fill group form
