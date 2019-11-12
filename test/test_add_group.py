49189891fea4c9a1fe65e1105de9a9fb1eabc2aa
cronnoss <karasevevg@gmail.com>
2019-11-12 17:11:36 +1000

    implement sorting lists

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,17 +3,22 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="pytest1", header="pytest2", footer="pytest3"))
+    group = Group(name="pytest1", header="pytest2", footer="pytest3")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-
-#   for testing ensure_login
-#   app.session.logout()
+    #   for testing ensure_login
+    #   app.session.logout()
 
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
