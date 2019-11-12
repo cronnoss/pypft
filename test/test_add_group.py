a9465f1b3476e6d53ef919604514a11a0c7b1b47
cronnoss <karasevevg@gmail.com>
2019-11-12 11:44:02 +1000

    loading list

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,10 +2,18 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="pytest1", header="pytest2", footer="pytest3"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+
+
 #   for testing ensure_login
-#    app.session.logout()
+#   app.session.logout()
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
