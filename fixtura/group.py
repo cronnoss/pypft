e6acf29a41223f7afb5d377ba97b21e92e112b44
cronnoss <karasevevg@gmail.com>
2019-11-12 18:39:13 +1000

    optimization with cache and hash

--- fixtura/group.py
+++ fixtura/group.py
@@ -20,6 +20,7 @@ class GroupHelper:
         # submit group creation
         wd.find_element(By.NAME, "submit").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -41,6 +42,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element(By.NAME, "delete").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def select_first_group(self):
         wd = self.app.wd
@@ -57,6 +59,7 @@ class GroupHelper:
         # submit modification
         wd.find_element(By.NAME, "update").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def open_groups_page(self):
         wd = self.app.wd
@@ -68,12 +71,15 @@ class GroupHelper:
         self.open_groups_page()
         return len(wd.find_elements(By.XPATH, "//input[@name='selected[]']"))
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups = []
-        for element in wd.find_elements(By.CSS_SELECTOR, "span.group"):
-            text = element.text
-            id = element.find_element(By.NAME, "selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cache = []
+            for element in wd.find_elements(By.CSS_SELECTOR, "span.group"):
+                text = element.text
+                id = element.find_element(By.NAME, "selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,20 +5,19 @@ def test_add_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="pytest1", header="pytest2", footer="pytest3")
     app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
     #   for testing ensure_login
     #   app.session.logout()
 
-
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+# def test_add_empty_group(app):
+#    old_groups = app.group.get_group_list()
+#    group = Group(name="", header="", footer="")
+#    app.group.create(group)
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) + 1 == len(new_groups)
+#    old_groups.append(group)
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
