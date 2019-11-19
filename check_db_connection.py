c4d05ff7feacacf768b94c26cf4362deaa2521f9
cronnoss <karasevevg@gmail.com>
2019-11-19 14:14:21 +1000

    implement relations between contacts in group

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,14 +1,40 @@
 from fixtura.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_not_in_group(Group(id="216"))
     for item in l:
         print(item)
     print(len(l))
 finally:
     pass
+
+# from fixtura.orm import ORMFixture
+# from model.group import Group
+
+# db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+# try:
+# l = db.get_contacts_in_group(Group(id="216"))
+# for item in l:
+# print(item)
+# print(len(l))
+# finally:
+# pass
+
+# from fixtura.orm import ORMFixture
+
+# db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+# try:
+# l = db.get_contact_list()
+# for item in l:
+# print(item)
+# print(len(l))
+# finally:
+# pass
 
 # from fixtura.orm import ORMFixture
 
